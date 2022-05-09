be6204029b318922b6f4f77d7fa7cf4455e6d332
Sophie Déziel <courrier@sophiedeziel.com>
2022-05-09 11:46:14 -0400

    Créé l'index des articles

---
+++ src/routes/blogs/[handle].server.jsx
@@ -0,0 +1,66 @@
+import {
+  flattenConnection,
+  useShopQuery,
+  useRouteParams,
+} from '@shopify/hydrogen';
+import gql from 'graphql-tag';
+import ArticleCard from '../../components/ArticleCard';
+
+import Layout from '../../components/Layout.server';
+
+export default function Blogs() {
+  const {handle} = useRouteParams();
+
+  const {
+    data: {blog},
+  } = useShopQuery({
+    query: QUERY,
+    variables: {
+      handle,
+    },
+    preload: true,
+  });
+
+  const articles = flattenConnection(blog.articles);
+
+  return (
+    <Layout>
+      <h1 className="font-bold text-4xl md:text-5xl text-gray-900 mb-6 mt-6">
+        {blog.title}
+      </h1>
+      <p className="text-sm text-gray-500 mt-5 mb-5">
+        {articles.length} {articles.length > 1 ? 'articles' : 'article'}
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
+        {articles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+const QUERY = gql`
+  query Blog($handle: String!) {
+    blog(handle: $handle) {
+      title
+      articles(first: 20) {
+        edges {
+          node {
+            title
+            excerptHtml
+            image {
+              id
+              altText
+              height
+              url
+              width
+            }
+            publishedAt
+            onlineStoreUrl
+          }
+        }
+      }
+    }
+  }
+`;
