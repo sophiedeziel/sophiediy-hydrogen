0d7475a0dd636e6ecbfa46e14f51f9b053b3279e
Sophie Déziel <courrier@sophiedeziel.com>
2022-05-09 10:38:50 -0400

    Installed and configured hydrogen

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  defaultLocale: 'fr-ca',
+  storeDomain: 'sophie-diy.myshopify.com',
+  storefrontToken: 'c1a35016f963fbb95966255e10476e8e',
+  storefrontApiVersion: 'unstable',
+};
